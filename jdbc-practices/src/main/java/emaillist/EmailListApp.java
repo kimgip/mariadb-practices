79a62cd17ebe485ba2d17a2b2e4a5a34d3601d52
kimgip <gippeum0102@gmail.com>
2024-05-20 10:20:46 +0900

    emailList insert, delete, list control 개발

--- jdbc-practices/src/main/java/emaillist/EmailListApp.java
+++ jdbc-practices/src/main/java/emaillist/EmailListApp.java
@@ -1,11 +1,17 @@
 package emaillist;
 
+import java.util.List;
 import java.util.Scanner;
 
-public class EmailListApp {
+import emaillist.dao.EmaillistDao;
+import emaillist.vo.EmaillistVo;
 
+public class EmailListApp {
+	private static Scanner scanner = null;
+	private static EmaillistDao emaillistdao = new EmaillistDao();
+	
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		scanner = new Scanner(System.in);
 		
 		while(true) {
 			System.out.print("(l)ist (d)elete (i)nsert (q)uit > ");
@@ -21,18 +27,44 @@ public class EmailListApp {
 				break;
 			}
 		}
+		
+		if (scanner!= null) {
+			scanner.close();
+		}
 	}
 
 	private static void doInsert() {
-		System.out.println("doInsert");
+		System.out.print("성:");
+		String firstName = scanner.nextLine();
+		
+		System.out.print("이름:");
+		String lastName = scanner.nextLine();
+		
+		System.out.print("이메일:");
+		String email = scanner.nextLine();
+		
+		EmaillistVo vo = new EmaillistVo();
+		vo.setFirstName(firstName);
+		vo.setLastName(lastName);
+		vo.setEmail(email);
+		
+		emaillistdao.insert(vo);
+		
+		doList();
 	}
 
 	private static void doDelete() {
-		System.out.println("doDelete");
+		System.out.println("이메일:");
+		String email = scanner.nextLine();
+		
+		emaillistdao.deleteByEmail(email);
 	}
 
 	private static void doList() {
-		System.out.println("doList");
+		List<EmaillistVo> list = emaillistdao.findAll();
+		for(EmaillistVo vo : list) {
+			System.out.println(vo.toString());
+		}
 	}
 
 }
